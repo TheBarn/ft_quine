2b113e1b0d378607c89976e4b9d6e060131b431e
Bastien ARNOUT <barnout@e3r4p3.42.fr>
2017-12-09 14:44:00 +0100

    from 100 to 5

--- Sully.c
+++ Sully.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main()
 {
-	int i = 100;
+	int i = 5;
 	char *rep = "#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%c%cint i = %d;%c%cchar *rep = %c%s%c;%c%cchar *name;%c%cchar *cmd;%c%cFILE *f;%c%c%casprintf(&name, %cSully_%%d.c%c, i);%c%casprintf(&cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d && ./Sully_%%d%c, name, i, i);%c%cf = fopen(name, %cw%c);%c%cfprintf(f, rep, 10, 10, 10, 10, 9, i-1, 10, 9, 34, rep, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10);%c%cfclose(f);%c%cfree(name);%c%cif(i > 0)%c%c%csystem(cmd);%c%cfree(cmd);%c}%c";
 	char *name;
 	char *cmd;
