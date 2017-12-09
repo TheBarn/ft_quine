dd5eb5fd2980284634bce46d42d4ac223adcff4c
Bastien ARNOUT <barnout@e3r4p3.42.fr>
2017-12-09 15:20:34 +0100

    Sully with __FILE__ macro

--- Sully.c
+++ Sully.c
@@ -3,15 +3,18 @@
 int main()
 {
 	int i = 5;
-	char *rep = "#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%c%cint i = %d;%c%cchar *rep = %c%s%c;%c%cchar *name;%c%cchar *cmd;%c%cFILE *f;%c%c%casprintf(&name, %cSully_%%d.c%c, i);%c%casprintf(&cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d && ./Sully_%%d%c, name, i, i);%c%cf = fopen(name, %cw%c);%c%cfprintf(f, rep, 10, 10, 10, 10, 9, i-1, 10, 9, 34, rep, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10);%c%cfclose(f);%c%cfree(name);%c%cif(i > 0)%c%c%csystem(cmd);%c%cfree(cmd);%c}%c";
+	char *rep = "#include <stdio.h>%c#include <stdlib.h>%cint main()%c{%c%cint i = %d;%c%cchar *rep = %c%s%c;%c%cchar *name;%c%cchar *cmd;%c%cFILE *f;%c%cint d = i;%c%c%cif (__FILE__[5] == '_')%c%c%cd = i - 1;%c%casprintf(&name, %cSully_%%d.c%c, d);%c%casprintf(&cmd, %cgcc -Wall -Wextra -Werror %%s -o Sully_%%d && ./Sully_%%d%c, name, d, d);%c%cf = fopen(name, %cw%c);%c%cfprintf(f, rep, 10, 10, 10, 10, 9, d, 10, 9, 34, rep, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10);%c%cfclose(f);%c%cfree(name);%c%cif(i > 0)%c%c%csystem(cmd);%c%cfree(cmd);%c}%c";
 	char *name;
 	char *cmd;
 	FILE *f;
+	int d = i;
 
-	asprintf(&name, "Sully_%d.c", i);
-	asprintf(&cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d && ./Sully_%d", name, i, i);
+	if (__FILE__[5] == '_')
+		d = i - 1;
+	asprintf(&name, "Sully_%d.c", d);
+	asprintf(&cmd, "gcc -Wall -Wextra -Werror %s -o Sully_%d && ./Sully_%d", name, d, d);
 	f = fopen(name, "w");
-	fprintf(f, rep, 10, 10, 10, 10, 9, i-1, 10, 9, 34, rep, 34, 10, 9, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10);
+	fprintf(f, rep, 10, 10, 10, 10, 9, d, 10, 9, 34, rep, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10);
 	fclose(f);
 	free(name);
 	if(i > 0)
