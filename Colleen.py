3e27632b83a627f1585ce7f4091f847c57a8c877
Bastien ARNOUT <barnout@e3r4p3.42.fr>
2017-12-09 20:08:17 +0100

    first Colleen python

---
+++ Colleen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#first comment
+
+def main()
+{
+        a = "#!/usr/bin/python3{:c}#first comment{:c}{:c}{:c}def main(){:c}{{:c}{:c}a = {:c}{:s}{:c};{:c}{:c}print(a){:c}{:c}#second comment{:c}}{:c}{:c}if __NAME__ == {:c}__main__{:c}:{:c}{:c}main()";
+    print(a)
+    #second comment
+}
+
+if __NAME__ == "__main__":
+    main()
