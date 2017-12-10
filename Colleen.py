0937d45145b2a14d5d6cfab24d44df4d5d1197c9
Bastien ARNOUT <barnout@e3r5p5.42.fr>
2017-12-10 17:37:39 +0100

    Colleen in python

--- Colleen.py
+++ Colleen.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 #first comment
 
-def main()
-{
-        a = "#!/usr/bin/python3{:c}#first comment{:c}{:c}{:c}def main(){:c}{{:c}{:c}a = {:c}{:s}{:c};{:c}{:c}print(a){:c}{:c}#second comment{:c}}{:c}{:c}if __NAME__ == {:c}__main__{:c}:{:c}{:c}main()";
-    print(a)
-    #second comment
-}
+def print_c():
+	a = "#!/usr/bin/python3{:c}#first comment{:c}{:c}def print_c():{:c}{:c}a = {:c}{:s}{:c};{:c}{:c}print(a.format(10, 10, 10, 10, 9, 34, a, 34, 10, 9, 10, 10, 10, 9, 10, 9, 10, 10, 34, 34, 10, 9)){:c}{:c}def main():{:c}{:c}#second comment{:c}{:c}print_c(){:c}{:c}if __name__ == {:c}__main__{:c}:{:c}{:c}main()";
+	print(a.format(10, 10, 10, 10, 9, 34, a, 34, 10, 9, 10, 10, 10, 9, 10, 9, 10, 10, 34, 34, 10, 9))
 
-if __NAME__ == "__main__":
-    main()
+def main():
+	#second comment
+	print_c()
+
+if __name__ == "__main__":
+	main()
